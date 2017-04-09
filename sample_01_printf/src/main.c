bc6fbef3e8dd38a2a3f2c5d859293532a9014ee5
anchorz <andreas.ziermann@mrboot.de>
2017-04-09 11:02:35 +0800

    samples testen

--- sample_01_printf/src/main.c
+++ sample_01_printf/src/main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 char *str = "text";
-unsigned int w1;
+unsigned char w1;
 unsigned int w2 = 0x6543;
 const unsigned int w3 = 0x1234;
 char c1 = 'T';
 char c2 = '2';
+float f=5.6f;
 
 int main() {
     printf("  initialized string str:%s\n", str);
@@ -13,6 +14,7 @@ int main() {
     printf("  initialized word    w3:0x%04x\n", w3);
     printf("  initialized char    c1:%c\n", c1);
     printf("  initialized char    c2:%c\n", c2);
-    printf("uninitialized word    w1:0x%04x\n", w1);
+    printf("uninitialized char    w1:0x%02x\n", w1);
+    printf("  initialized float   f :%f\n", f);
     return 0;
 }
